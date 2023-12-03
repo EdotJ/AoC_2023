8e865ecf492e0093e94b44982291455159612962
EdotJ <eligijus.jocius+github@gmail.com>
2023-12-03 20:37:41 +0200

    Clean up part 1

--- day3/src/day3.clj
+++ day3/src/day3.clj
@@ -42,23 +42,6 @@
 (defn part1 [lines]
   (apply concat (map-indexed (fn [idx line] (find-numbers line (get lines (- idx 1)) (get lines (+ idx 1)))) lines)))
 
-(defn find-gear-numbers [line prev next]
-  (let [numbers (apply concat (distinct (re-seq digit-pattern line)))
-        indices (apply concat (map #(find-indices (remove-unnecessary line %) %) numbers))]
-    indices))
-
-(defn is-gear [gear-idx line prev nextt]
-  (let [search-area (map #(get-line-within-bounds (- gear-idx 1) (+ gear-idx 1) %) (filter #(not (nil? %)) [prev line next]))]
-    (= 2 (count (apply concat (map #(re-seq digit-pattern %) search-area))))))
-
-(defn find-gears-for-line [line prev next]
-  (let [gear-indices (filter #(true? (is-gear (:idx %) line prev next)) (find-indices line "*"))]
-    (find-gear-numbers line prev next)))
-
-(defn find-gears [lines]
-  (apply concat (map #(find-indices % "*") lines)))
-
 (defn solve [opts] (let [input (string/split (slurp "input.txt") #"\n")]
-                    ;;  (println (reduce + (map #(Integer/parseInt (:num %)) (part1 input))))
-                     (println (find-gears-for-line (get input 1) (get input 0) (get input 2)))
+                     (println (reduce + (map #(Integer/parseInt (:num %)) (part1 input))))
                      ))
