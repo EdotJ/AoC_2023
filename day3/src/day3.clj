d8e43d50143d6a12dd3139a6ca3b4b4beb760e90
EdotJ <eligijus.jocius+github@gmail.com>
2023-12-03 20:46:24 +0200

    Join into single file

--- day3/src/day3.clj
+++ day3/src/day3.clj
@@ -13,13 +13,13 @@
         ret-val
         (recur (string/replace ret-val (first numbers) (apply str (repeat (count (first numbers)) "."))) (drop 1 numbers))))))
 
-(defn find-indices [line number]
+(defn find-indices [line number line-num]
   (loop [loop-line line
          found-indices []]
     (let [potential-idx (string/index-of loop-line number)]
       (if (nil? potential-idx)
         found-indices
-        (recur (string/replace-first loop-line number (apply str (repeat (count number) "."))) (concat found-indices [{:num number :idx (string/index-of loop-line number)}]))))))
+        (recur (string/replace-first loop-line number (apply str (repeat (count number) "."))) (concat found-indices [{:num number :idx (string/index-of loop-line number) :line-num line-num :end-idx (+ (string/index-of loop-line number) (- (count number) 1))}]))))))
 
 (defn get-line-within-bounds [start-idx end-idx line]
   (if (> start-idx 0)
@@ -34,14 +34,45 @@
   (let [search-area (map #(get-line-within-bounds start-idx end-idx %) (filter #(not (nil? %)) [prev-line line next-line]))]
     (some #(not (nil? %)) (map #(re-find symbol-patttern %) search-area))))
 
-(defn find-numbers [line prev next]
-  (let [numbers (distinct (re-seq digit-pattern line))
-        indices (apply concat (map #(find-indices (remove-unnecessary line %) %) numbers))]
+(defn find-numbers [line line-num prev next]
+  (let [numbers (distinct (re-seq digit-pattern line))]
+    (apply concat (map #(find-indices (remove-unnecessary line %) % line-num) numbers))))
+
+(defn find-part-numbers [line line-num prev next]
+  (let [indices (find-numbers line line-num prev next)]
     (filter #(is-part-number (- (:idx %) 1) (+ 1 (count (:num %)) (:idx %)) line prev next) indices)))
 
-(defn part1 [lines]
-  (apply concat (map-indexed (fn [idx line] (find-numbers line (get lines (- idx 1)) (get lines (+ idx 1)))) lines)))
+(defn get-all-part-numbers [lines]
+  (apply concat (map-indexed (fn [idx line] (find-part-numbers line idx (get lines (- idx 1)) (get lines (+ idx 1)))) lines)))
+
+(defn part-one [lines]
+  (println (reduce + (map #(Integer/parseInt (:num %)) (get-all-part-numbers lines)))))
+
+(defn is-adjacent-num [gear num]
+  (let [gear-range [(- (:idx gear) 1) (+ (:idx gear) 1)]
+        num-range [(:idx num) (:end-idx num)]]
+    (and (<= (first gear-range) (second num-range)) (<= (first num-range) (second gear-range)))))
+
+(defn find-numbers-for-gear [gear all-nums]
+  (let [gear-line (:line-num gear)
+        nums-by-line (filter #(<= (- gear-line 1) (:line-num %) (+ gear-line 1)) all-nums)]
+    (filter #(is-adjacent-num gear %) nums-by-line)))
+
+(defn find-gears [line line-num all-nums]
+  (let [potential-gears (find-indices line "*" line-num)]
+    (if (empty? potential-gears)
+      []
+      (let [gear-nums (map #(find-numbers-for-gear % all-nums) potential-gears)]
+        (filter #(= (count %) 2) gear-nums)))))
+
+(defn find-gear-ratio [gear-nums]
+  (* (Integer/parseInt (:num (first gear-nums))) (Integer/parseInt (:num (last gear-nums)))))
+
+(defn part-two [lines]
+  (let [all-nums (apply concat (map-indexed (fn [idx line] (find-numbers line idx (get lines (- idx 1)) (get lines (+ idx 1)))) lines))
+        gears  (apply concat (filter #(not (empty? %)) (map-indexed (fn [idx line] (find-gears line idx all-nums)) lines)))]
+    (println (reduce + (map find-gear-ratio gears)))))
 
 (defn solve [opts] (let [input (string/split (slurp "input.txt") #"\n")]
-                     (println (reduce + (map #(Integer/parseInt (:num %)) (part1 input))))
-                     ))
+                     (part-one input)
+                     (part-two input)))
